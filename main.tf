75e7afdb6d29f690f80f2142097d688d24a8fcda
Sean Sundberg <seansund@us.ibm.com>
2020-05-29 01:36:50 -0500

    Removes helm3 reference

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
+provider "helm" {
+  version = ">= 1.1.1"
+
+  kubernetes {
+    config_path = var.cluster_config_file
+  }
+}
+
 locals {
   tmp_dir       = "${path.cwd}/.tmp"
   chart         = "${path.module}/charts/pact-broker"
-  ingress_host  = "pact.${var.cluster_ingress_hostname}"
+  ingress_host  = "pact-${var.releases_namespace}.${var.cluster_ingress_hostname}"
   ingress_url   = "http://${local.ingress_host}"
   database_type = "sqlite"
   database_name = "pactbroker.sqlite"
@@ -10,28 +18,64 @@ locals {
   cluster_type  = var.cluster_type == "kubernetes" ? "kubernetes" : "openshift"
 }
 
-resource "null_resource" "pactbroker_release" {
-  triggers = {
-    kubeconfig_iks     = var.cluster_config_file
-    releases_namespace = var.releases_namespace
+resource "helm_release" "pactbroker" {
+  chart     = local.chart
+  name      = "pact-broker"
+  namespace = var.releases_namespace
+  disable_openapi_validation = true
+
+  set {
+    name  = "ingress.enabled"
+    value = var.cluster_type == "kubernetes" ? "true" : "false"
   }
 
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/deploy-pactbroker.sh ${local.chart} ${self.triggers.releases_namespace} ${local.ingress_host} ${local.database_type} ${local.database_name} ${var.tls_secret_name}"
-
-    environment = {
-      KUBECONFIG = self.triggers.kubeconfig_iks
-      TMP_DIR        = local.tmp_dir
-      CLUSTER_TYPE   = local.cluster_type
-    }
+  set {
+    name  = "route.enabled"
+    value = var.cluster_type == "kubernetes" ? "false" : "true"
   }
 
-  provisioner "local-exec" {
-    when    = destroy
-    command = "${path.module}/scripts/destroy-pactbroker.sh ${self.triggers.releases_namespace}"
+  set {
+    name  = "ingress.hosts.0.host"
+    value = local.ingress_host
+  }
 
-    environment = {
-      KUBECONFIG = self.triggers.kubeconfig_iks
-    }
+  set {
+    name  = "ingress.tls[0].secretName"
+    value = var.tls_secret_name
+  }
+
+  set {
+    name  = "ingress.tls[0].hosts[0]"
+    value = local.ingress_host
+  }
+
+  set {
+    name  = "database.type"
+    value = local.database_type
+  }
+
+  set {
+    name  = "database.name"
+    value = local.database_name
+  }
+}
+
+resource "helm_release" "pactbroker-config" {
+  depends_on = [helm_release.pactbroker]
+
+  name         = "pactbroker-config"
+  repository   = "https://ibm-garage-cloud.github.io/toolkit-charts/"
+  chart        = "tool-config"
+  namespace    = var.releases_namespace
+  force_update = true
+
+  set {
+    name  = "name"
+    value = "pactbroker"
+  }
+
+  set {
+    name  = "url"
+    value = local.ingress_url
   }
 }

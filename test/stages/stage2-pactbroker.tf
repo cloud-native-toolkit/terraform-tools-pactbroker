0f91cf3476210d9b3a82b01a546f5d3b4d8f4921
Sean Sundberg <seansund@us.ibm.com>
2021-04-05 08:02:34 -0500

    Adds ocp-vpn module to dependency list
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-pactbroker.tf
+++ test/stages/stage2-pactbroker.tf
@@ -1,9 +1,9 @@
 module "dev_tools_pactbroker" {
   source = "github.com/ibm-garage-cloud/terraform-tools-pactbroker.git"
 
-  cluster_type                                  = module.dev_cluster.type_code
-  cluster_ingress_hostname                      = module.dev_cluster.ingress_hostname
   cluster_config_file                           = module.dev_cluster.config_file_path
-  tls_secret_name                               = module.dev_cluster.tls_secret_name
+  cluster_type                                  = module.dev_cluster.platform.type_code
+  cluster_ingress_hostname                      = module.dev_cluster.platform.ingress
+  tls_secret_name                               = module.dev_cluster.platform.tls_secret
   releases_namespace                            = module.dev_capture_state.namespace
 }

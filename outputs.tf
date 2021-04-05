08638590ac1062175df7e27cccddfcb46b1fa138
Sean Sundberg <seansund@us.ibm.com>
2021-04-05 09:57:36 -0500

    Updates output dependencies
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,23 @@
 output "ingress_host" {
   description = "The ingress host for the Pact Broker instance"
   value       = local.ingress_host
-  depends_on  = [null_resource.pactbroker_release]
+  depends_on  = [helm_release.pactbroker]
 }
 
 output "ingress_url" {
   description = "The ingress url for the Pact Broker instance"
   value       = local.ingress_url
-  depends_on  = [null_resource.pactbroker_release]
+  depends_on  = [helm_release.pactbroker]
 }
 
 output "secret_name" {
   description = "The name of the secret created to store the credentials"
   value       = local.secret_name
-  depends_on  = [null_resource.pactbroker_release]
+  depends_on  = [helm_release.pactbroker]
 }
 
 output "config_name" {
   description = "The name of the ConfigMap created to store the url"
   value       = local.config_name
-  depends_on  = [null_resource.pactbroker_release]
+  depends_on  = [helm_release.pactbroker]
 }

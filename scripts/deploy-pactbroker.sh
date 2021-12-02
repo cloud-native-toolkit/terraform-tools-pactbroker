ddcde374e908b099db68bc7a58abc090148750c1
Andrew Trice <amtrice@us.ibm.com>
2021-12-02 11:27:17 -0500

    more debugging commands
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- scripts/deploy-pactbroker.sh
+++ scripts/deploy-pactbroker.sh
@@ -81,8 +81,8 @@ kubectl apply -n ${NAMESPACE} -f ${OUTPUT_YAML} --validate=false
 if [[ "${CLUSTER_TYPE}" == "openshift" ]] || [[ "${CLUSTER_TYPE}" == "ocp3" ]] || [[ "${CLUSTER_TYPE}" == "ocp4" ]]; then
   sleep 5
 
-oc get route pact-broker -n "${NAMESPACE}" -o=json
-oc get route pact-broker -n "${NAMESPACE}" -o=jsonpath="{ .spec.host }"
+# oc get route pact-broker -n "${NAMESPACE}" -o=json
+# oc get route pact-broker -n "${NAMESPACE}" -o=jsonpath="{ .spec.host }"
 
 
   PACTBROKER_HOST=$(oc get route pact-broker -n "${NAMESPACE}" -o=jsonpath="{ .spec.host }")

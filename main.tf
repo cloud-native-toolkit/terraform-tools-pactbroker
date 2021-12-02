fe15f1e6e585eb65e8dfb13bfff31c704c24a304
Andrew Trice <amtrice@us.ibm.com>
2021-12-02 12:25:16 -0500

    went back to the helm provider for comparison... and ... its working now
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- main.tf
+++ main.tf
@@ -71,50 +71,6 @@ resource "helm_release" "pactbroker" {
 
 
 
-# resource null_resource pactbroker_helm {
-#   depends_on = [null_resource.delete-consolelink]
-
-#   triggers = {
-#     namespace = var.releases_namespace
-#     name = "pact-broker"
-#     chart = "toolkit-charts/pact-broker"
-#     ingress_host = local.ingress_host
-#     ingress_enabled = var.cluster_type == "kubernetes" ? "true" : "false"
-#     route_enabbled = var.cluster_type == "kubernetes" ? "false" : "true"
-#     database_type = local.database_type
-#     database_name = local.database_name
-#     tls_secret_name = var.tls_secret_name
-#     kubeconfig = var.cluster_config_file
-#     tmp_dir = local.tmp_dir
-#     bin_dir = local.bin_dir
-#     cluster_type = var.cluster_type
-#   }
-
-#   provisioner "local-exec" {
-#     command = "${path.module}/scripts/deploy-pactbroker.sh ${self.triggers.chart} ${self.triggers.namespace} ${self.triggers.ingress_host} ${self.triggers.database_type} ${self.triggers.database_name} ${self.triggers.tls_secret_name} ${self.triggers.ingress_enabled} ${self.triggers.route_enabbled} ${self.triggers.cluster_type}"
-
-#     environment = {
-#       KUBECONFIG = self.triggers.kubeconfig
-#       TMP_DIR = self.triggers.tmp_dir
-#       BIN_DIR = self.triggers.bin_dir
-#     }
-#   }
-
-#   provisioner "local-exec" {
-#     when = destroy
-
-#     command = "${path.module}/scripts/destroy-pactbroker.sh ${self.triggers.namespace} ${self.triggers.name} ${self.triggers.chart}"
-
-#     environment = {
-#       KUBECONFIG = self.triggers.kubeconfig
-#       TMP_DIR = self.triggers.tmp_dir
-#       BIN_DIR = self.triggers.bin_dir
-#     }
-#   }
-# }
-
-
-
 resource "null_resource" "delete-consolelink" {
   count = var.cluster_type != "kubernetes" ? 1 : 0
 

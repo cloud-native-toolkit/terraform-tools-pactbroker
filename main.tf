2b9bb26b55d3ddb096955ae8a5039a2044177705
Andrew Trice <amtrice@us.ibm.com>
2021-12-02 21:53:00 -0500

    cleaned up commented out code
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- main.tf
+++ main.tf
@@ -22,54 +22,6 @@ resource null_resource print_toolkit_namespace {
   }
 }
 
-# resource "helm_release" "pactbroker" {
-#   depends_on = [null_resource.print_toolkit_namespace]
-
-#   chart      = "pact-broker"
-#   name       = "pact-broker"
-#   namespace  = var.releases_namespace
-#   disable_openapi_validation = true
-#   repository = "https://charts.cloudnativetoolkit.dev"
-#   version    = "0.2.1"
-  
-
-#   set {
-#     name  = "ingress.enabled"
-#     value = var.cluster_type == "kubernetes" ? "true" : "false"
-#   }
-
-#   set {
-#     name  = "route.enabled"
-#     value = var.cluster_type == "kubernetes" ? "false" : "true"
-#   }
-
-#   set {
-#     name  = "ingress.hosts.0.host"
-#     value = local.ingress_host
-#   }
-
-#   set {
-#     name  = "ingress.tls[0].secretName"
-#     value = var.tls_secret_name
-#   }
-
-#   set {
-#     name  = "ingress.tls[0].hosts[0]"
-#     value = local.ingress_host
-#   }
-
-#   set {
-#     name  = "database.type"
-#     value = local.database_type
-#   }
-
-#   set {
-#     name  = "database.name"
-#     value = local.database_name
-#   }
-# }
-
-
 
 resource "null_resource" "delete-consolelink" {
   count = var.cluster_type != "kubernetes" ? 1 : 0
@@ -82,35 +34,6 @@ resource "null_resource" "delete-consolelink" {
     }
   }
 }
-
-# resource "helm_release" "pactbroker-config" {
-#   depends_on = [helm_release.pactbroker, null_resource.delete-consolelink]
-
-#   name         = "pactbroker-config"
-#   repository   = "https://charts.cloudnativetoolkit.dev"
-#   chart        = "tool-config"
-#   namespace    = var.releases_namespace
-#   force_update = true
-
-#   set {
-#     name  = "name"
-#     value = "pactbroker"
-#   }
-
-#   set {
-#     name  = "privateUrl"
-#     value = local.service_url
-#   }
-
-#   set {
-#     name  = "applicationMenu"
-#     value = false
-#   }
-# }
-
-
-
-
 
 resource null_resource pactbroker_helm {
   depends_on = [null_resource.delete-consolelink]
